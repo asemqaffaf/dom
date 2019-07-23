bbca8eade671cf09e408392079fc29a503ae3daf
asemqaffaf <asem@qaffaf.com>
2019-07-23 22:29:06 +0300

    till exercise8

--- exercise8.js
+++ exercise8.js
@@ -1,19 +1,33 @@
 function initGrid() {
-    // collect colors in an array
-    var colors = [];
-    var range = ["00", "33", "66", "99", "cc", "ff"];
+  // collect colors in an array
+  var colors = [];
+  var range = ["00", "33", "66", "99", "cc", "ff"];
 
-    for (var r = 0; r < range.length; r++) {
-        for (var g = 0; g < range.length; g++) {
-            for (var b = 0; b < range.length; b++) {
-                colors.push("#" + range[r] + range[g] + range[b]);
-            }
-        }
+  for (var r = 0; r < range.length; r++) {
+    for (var g = 0; g < range.length; g++) {
+      for (var b = 0; b < range.length; b++) {
+        colors.push("#" + range[r] + range[g] + range[b]);
+      }
     }
+  }
+  var mainDiv = document.getElementById("colors");
+  //   mainDiv.onclick = colorPicker2("colors[i]");
 
-    // TODO complete the rest
+  for (let i = 0; i < colors.length; i++) {
+    var innerDiv = document.createElement("div");
+    innerDiv.style.backgroundColor = colors[i];
+    innerDiv.className = "choice";
+    innerDiv.addEventListener("click", colorPicker2);
+    mainDiv.appendChild(innerDiv);
+  }
 }
-
-window.onload = function () {
-    initGrid();
+function colorPicker2() {
+  var color = this.style.backgroundColor;
+  var mainSelected = document.getElementById("selected");
+  mainSelected.style.backgroundColor = color;
+  mainSelected.innerHTML = color;
+  //   console.log(color);
 }
+window.onload = function() {
+  initGrid();
+};
--- game.js
+++ game.js
@@ -1,1 +1,10 @@
 /* TODO implement game logic here */
+
+var arr1 = [[" ", " ", " ", " "], "*", "*", "*", "*"];
+
+for (let i of arr1) {
+  for (let j = 0; j < i[0].length; j++) {
+    console.log(j);
+  }
+  console.log(i);
+}
